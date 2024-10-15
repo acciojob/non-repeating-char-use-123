6e4873b790158ef37278a8a8d86e4d8898b7a580
use-123 <anmolbejugam@gmail.com>
2024-10-15 15:28:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+	const charCount = new Map();
+
+    for (let i = 0; i < str.length; i++){
+        const char = str[i];
+        charCount.set(char, (charCount.get(char || 0) + 1));
+    }
+
+    for (let i = 0; i < str.length; i++){
+        if (charCount.get(str[i]) == 1){
+            return str[i];
+        }
+    }
+
+    return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
